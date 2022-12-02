948c82706793a5927f0d966607511f4339de3ad9
brokenhalo420 <m.nikolov1501@gmail.com>
2022-12-02 19:12:10 +0200

    innitial skeleton created

---
+++ News/Worker/NewsWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Worker
+{
+    public class NewsWorker: BackgroundService
+    {
+        private readonly ILogger<NewsWorker> _logger;
+
+        public NewsWorker(ILogger<NewsWorker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
